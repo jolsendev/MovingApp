4e824506f454150d43147b0460759df0f375a726
jolsendev <jolsendev@gmail.com>
2017-09-19 20:57:04 -0600

    added nytapikey

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -12,9 +12,15 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr+', '+cityStr;
 
+    $greeting.text("So you want to live at "+address+"?");
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+    $body.append('<img class="bgimg" src="'+streetviewUrl+'">');
     // YOUR CODE GOES HERE!
-
+    //47396d0d5b384154b450128d3b5873e3
     return false;
 };
 
